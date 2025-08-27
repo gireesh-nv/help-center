2d844dd6bdd7f238c7e6ea0ed923902e1d8f4ec9
gireesh-nv <girishnu35@gmail.com>
2025-08-27 09:02:58 +0530

    Add python/simple_utils.py with small utility functions

---
+++ python/simple_utils.py
@@ -0,0 +1,13 @@
+# simple_utils.py - A tiny utility library
+
+def reverse_string(text):
+    """Reverses the characters in a string."""
+    return text[::-1]
+
+def count_words(sentence):
+    """Counts words in a sentence."""
+    return len(sentence.split())
+
+def celsius_to_fahrenheit(celsius):
+    """Converts Celsius temperature to Fahrenheit."""
+    return (celsius * 9/5) + 32
